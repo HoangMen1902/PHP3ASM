42f7069148858008b393e345e2b3a1d60456bab3
HoangMen1902 <hoangmen9918@gmail.com>
2025-03-27 13:27:34 +0700

    [Admin] - Bắt role Admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -18,6 +21,8 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
+
+  
     protected $fillable = [
         'name',
         'email',
@@ -42,6 +47,11 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
+
+     public function canAccessPanel(Panel $panel): bool
+     {
+        return $this->role === 2;
+     }
     protected function casts(): array
     {
         return [
