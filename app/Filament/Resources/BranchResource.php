39799c5b7e4c282ce5debcd2a09082fb3537815c
7CO <hoangmen9918@gmail.com>
2025-03-30 13:41:07 +0700

    [Admin] Thêm chức năng xóa cho các bảng

--- app/Filament/Resources/BranchResource.php
+++ app/Filament/Resources/BranchResource.php
@@ -75,6 +75,8 @@ class BranchResource extends Resource
         ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
