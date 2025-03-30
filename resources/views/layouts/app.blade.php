c14f3292a48787f4e44d4068d2932f7c5d5dced2
7CO <hoangmen9918@gmail.com>
2025-03-30 21:26:56 +0700

    [Client] Sửa nội dung Header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,14 +58,14 @@
                                         <ul id="navigation">
                                             <li class="active"><a href="/">Home</a></li>
                                             <li><a href="/about">About</a></li>
+                                            <li><a href="/products">Products</a></li>
                                             <li><a href="/service">Services</a></li>
-                                            <li><a href="/portfolio">Portfolio</a></li>
-                                            <li><a href="/blog">Blog</a>
-                                                <ul class="submenu">
-                                                    <li><a href="/blog-detail">Blog Details</a></li>
-                                                </ul>
-                                            </li>
                                             <li><a href="/contact">Contact</a></li>
+                                            <li><a
+                                                    href="{{ Auth::check() ? '/profile' : '/login' }}">{{ Auth::check() ? 'Profile' : 'Login' }}</a>
+                                            </li>
+
+                                            </li>
                                         </ul>
                                     </nav>
                                 </div>
@@ -151,10 +151,16 @@
                                 <!-- Form -->
                                 <div class="footer-form">
                                     <div id="mc_embed_signup">
-                                        <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscribe_form relative mail_part" novalidate="true">
-                                            <input type="email" name="EMAIL" id="newsletter-form-email" placeholder=" Email Address " class="placeholder hide-on-focus" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your email address'">
+                                        <form target="_blank"
+                                            action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
+                                            method="get" class="subscribe_form relative mail_part" novalidate="true">
+                                            <input type="email" name="EMAIL" id="newsletter-form-email"
+                                                placeholder=" Email Address " class="placeholder hide-on-focus"
+                                                onfocus="this.placeholder = ''"
+                                                onblur="this.placeholder = 'Your email address'">
                                             <div class="form-icon">
-                                                <button type="submit" name="submit" id="newsletter-submit" class="email_icon newsletter-submit button-contactForm">Send</button>
+                                                <button type="submit" name="submit" id="newsletter-submit"
+                                                    class="email_icon newsletter-submit button-contactForm">Send</button>
                                             </div>
                                             <div class="mt-10 info"></div>
                                         </form>
@@ -169,10 +175,14 @@
                         <div class="col-xl-9 col-lg-8">
                             <div class="footer-copy-right">
                                 <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                                    Copyright &copy;<script>
+                                    Copyright &copy;
+                                    <script>
                                         document.write(new Date().getFullYear());
-                                    </script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                                    </script> All rights reserved | This template is made with <i class="fa fa-heart"
+                                        aria-hidden="true"></i> by <a href="https://colorlib.com"
+                                        target="_blank">Colorlib</a>
+                                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                </p>
                             </div>
                         </div>
                         <div class="col-xl-3 col-lg-4">
