6d91847f2d9028911529804933696cd50950c4cc
Nguyễn Trung Sang <sangntpc08899@gmail.com>
2025-04-04 20:59:28 +0700

    [BE][Client] Cài đặt Breeze, Xử lý đăng ký - đăng nhập

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,17 @@
 <?php
 
-use App\Http\Middleware\Authenticate;
-use App\Http\Middleware\CartMiddleware;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\PortfolioController;
 use App\Http\Controllers\BlogController;
 use App\Livewire\Auth\Login;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\CartController;
-use App\Http\Controllers\CheckoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -26,21 +23,16 @@ Route::get('/about', [AboutController::class, 'index']);
 Route::get('/service', [ServiceController::class, 'index']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
-Route::get('/cart', [CartController::class, 'index']);
-Route::get('/checkout', [CheckoutController::class, 'index']);
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+                ->middleware('auth')
+                ->name('logout');
 
 
-Route::post('/add-to-cart', [CartController::class, 'cartInsert'])->middleware([Authenticate::class, CartMiddleware::class]);
-
-
-
-
-Route::get('/login', [Login::class, 'index'])->name('login');
 Route::get('/service', [ServiceController::class, 'index']);
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+    ->name('login');
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
@@ -50,4 +42,8 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
 });
 
-require __DIR__ . '/auth.php';
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
