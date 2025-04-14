ea83bd09388f7fee8ac118e2cd963835797765f2
7CO <hoangmen9918@gmail.com>
2025-04-14 22:59:47 +0700

    [Router] Route Fix cart

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Middleware\CartMiddleware;
+use App\Http\Middleware\CheckoutMiddleware;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
@@ -12,6 +16,7 @@ use App\Http\Controllers\PortfolioController;
 use App\Http\Controllers\BlogController;
 use App\Livewire\Auth\Login;
 use App\Http\Controllers\ProductController;
+use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -22,11 +27,13 @@ Route::get('/portfolio', [PortfolioController::class, 'index']);
 Route::get('/about', [AboutController::class, 'index']);
 Route::get('/service', [ServiceController::class, 'index']);
 Route::get('/products', [ProductController::class, 'index']);
+Route::get('/cart', [CartController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/checkout', [CheckoutController::class, 'index'])->middleware([CheckoutMiddleware::class, Authenticate::class]);
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->middleware('auth')
                 ->name('logout');
-
+Route::post('/add-to-cart', [CartController::class, 'cartInsert'])->middleware([Authenticate::class, CartMiddleware::class]);
 
 Route::get('/service', [ServiceController::class, 'index']);
 
