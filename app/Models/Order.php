48650880d6dd4705575aae2abc7041f572ea3054
7CO <hoangmen9918@gmail.com>
2025-04-17 04:03:22 +0700

    [Migration] Cập nhật Migration địa chỉ

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,11 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    protected $fillable = ['status', 'total_price', 'user_id', 'address'];
+    protected $fillable = ['status', 'total_price', 'user_id', 'address', 'address_username', 'address_phone'];
 
     protected $table = 'orders';
 
     public function orderDetails() {
         return $this->hasMany(OrderDetail::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
     }
 }
