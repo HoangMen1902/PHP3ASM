c5def969008336233327c3d467a0038abd7a09c9
Nguyễn Trung Sang <sangntpc08899@gmail.com>
2025-04-17 03:18:24 +0700

    Bổ sung chức năng thống kê

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    use HasFactory;
     protected $fillable = ['status', 'total_price', 'user_id', 'address', 'address_username', 'address_phone'];
 
     protected $table = 'orders';
