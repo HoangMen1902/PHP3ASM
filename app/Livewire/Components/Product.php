eb478bc2709596afb3681b3bf5c1f997b62cd8b3
Nguyễn Trung Sang <sangntpc08899@gmail.com>
2025-04-21 14:56:34 +0700

    [Client][BE] - Chức năng tìm kiếm sản phẩm

--- app/Livewire/Components/Product.php
+++ app/Livewire/Components/Product.php
@@ -9,11 +9,42 @@ use Livewire\Component;
 
 class Product extends Component
 {
+    public $search = '';
+    public $data;
+    public $categories;
+    public function searchProduct()
+    {
+        $this->data = ModelsProduct::select(
+            'products.*',
+            DB::raw('(SELECT MIN(price) FROM product_skus WHERE product_skus.product_id = products.id) AS min_price'),
+            DB::raw('(SELECT MAX(price) FROM product_skus WHERE product_skus.product_id = products.id) AS max_price')
+        )
+            ->where('name', 'like', '%' . $this->search . '%')
+            ->get();
 
+        $this->categories = Category::select(
+            'categories.*',
+            DB::raw('(SELECT COUNT(*) FROM products WHERE products.category_id = categories.id) AS count_products')
+        )->get();
+    }
     public function render()
     {
-        $products = ModelsProduct::select('products.*', DB::raw('(SELECT MIN(price) FROM product_skus WHERE product_skus.product_id = products.id) AS min_price'), DB::raw('(SELECT MAX(price) FROM product_skus WHERE product_skus.product_id = products.id) AS max_price'))->get();
-        $categories = Category::select('categories.*',  DB::raw('(SELECT COUNT(*) FROM products WHERE products.category_id = categories.id) AS count_products'))->get();
-        return view('livewire.components.product', ['data' => $products, 'categories' => $categories]);
+        
+        $products = $this->data ?? ModelsProduct::select(
+            'products.*',
+            DB::raw('(SELECT MIN(price) FROM product_skus WHERE product_skus.product_id = products.id) AS min_price'),
+            DB::raw('(SELECT MAX(price) FROM product_skus WHERE product_skus.product_id = products.id) AS max_price')
+        )->get();
+
+        $categories = $this->categories ?? Category::select(
+            'categories.*',
+            DB::raw('(SELECT COUNT(*) FROM products WHERE products.category_id = categories.id) AS count_products')
+        )->get();
+        $this->data = $products;
+        $this->categories = $categories;
+        return view('livewire.components.product', [
+            'data' => $products,
+            'categories' => $categories
+        ]);
     }
 }
